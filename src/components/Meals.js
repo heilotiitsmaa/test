c74dc140b95ea078d952e9651a136827e0bd9ac9
Heilo <heilo.tiitsmaa@voco.ee>
2025-04-02 15:48:17 +0300

    ül 3

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,4 +1,23 @@
+import { useEffect } from "react"
+
 const Meals = () => {
+    // useEffect to fetch data from backend
+    useEffect(() => {
+        const fetchMeals = async () => {
+            try {
+                const response = await fetch('http://localhost:3001/meals');
+                if (!response.ok) {
+                    throw new Error('Failed to fetch meals');
+                }
+                const data = await response.json();
+                console.log(data);
+                // setMeals(data); // Assuming you have a state to set the meals
+            } catch (error) {
+                console.error('Error fetching meals:', error);
+            }
+        };
+        fetchMeals();
+    }, []); // Empty dependency array to run once on component mount
     return (
         <ul id="meals">
             { 
