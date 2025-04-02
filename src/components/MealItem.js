6a065b0e5d08d1a5d38baac233eefa34f95f17ec
Heilo <heilo.tiitsmaa@voco.ee>
2025-04-02 16:06:29 +0300

    ül 5

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,11 +1,18 @@
+import React from 'react';
 const MealItem = (props) => {
+    
+    const formattedPrice = new Intl.NumberFormat('de-DE', {
+        style: 'currency',
+        currency: 'EUR', // Asenda oma valuutaga (nt EUR, GBP)
+      }).format(props.meal.price);
+
     return (
         <li className="meal-item" >
             <article>
                 <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name}/>
                 <div>
                     <h3>{props.meal.name}</h3>
-                    <p className='meal-item-price'>props.meal.price</p>
+                    <p className='meal-item-price'>{formattedPrice}</p>
                     <p className='meal-item-description'>{props.meal.description}</p>
                 </div>
                 <p>
